655d65f784194cc2aecf27d710c1ca815bf85e20
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2024-08-30 20:00:31 +0200

    Update the Nix flake (GHC 9.6).
    Change-Id: I5d987d15439e25e297c23f23e99ded4cc80ea34f

--- flake.nix
+++ flake.nix
@@ -12,20 +12,12 @@
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ] (system:
       let
         overlays = [ ];
 
         pkgs =
           import nixpkgs { inherit system overlays; config.allowBroken = true; };
-
-        co-log-src = pkgs.fetchgit
-          {
-            url = "https://github.com/kowainik/co-log";
-            sha256 = "sha256-XV1xh3aBRY2XwWjGRkd7F2DHf3zTeeFawHJzf05eaWE"; # pkgs.lib.fakeSha256;
-            rev = "72fbe394b437c698d574cd7604ad3f7f807383e0";
-            fetchSubmodules = false;
-          };
 
         project = returnShellEnv:
           pkgs.haskellPackages.developPackage {
@@ -40,21 +32,6 @@
             overrides = self: super: with pkgs.haskell.lib; {
               # Use callCabal2nix to override Haskell dependencies here
               # cf. https://tek.brick.do/K3VXJd8mEKO7
-
-              co-log-polysemy = pkgs.haskell.lib.dontCheck
-                (self.callCabal2nixWithOptions "co-log-polysemy" co-log-src "--subpath=co-log-polysemy" { });
-
-              doctest = pkgs.haskell.lib.dontCheck
-                (self.callHackage "doctest" "0.16.3" { });
-
-              optics = pkgs.haskell.lib.dontCheck
-                (self.callHackage "optics" "0.4" { });
-              optics-core = pkgs.haskell.lib.dontCheck
-                (self.callHackage "optics-core" "0.4" { });
-              optics-th = pkgs.haskell.lib.dontCheck
-                (self.callHackage "optics-th" "0.4" { });
-              optics-extra = pkgs.haskell.lib.dontCheck
-                (self.callHackage "optics-extra" "0.4" { });
             };
 
             modifier = drv:
